57e5bc938b39ae456f75691ecfaf40660060d7e8
darkstorn <inikishin@gmail.com>
2021-12-12 11:34:14 +0300

    initial commit

---
+++ pandora_trading_solutions_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pandora_trading_solutions_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pandora_trading_solutions_backend.settings')
+
+application = get_asgi_application()
